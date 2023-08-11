91d458cc255a14a590c062ea3995a47c71bf121b
saalen <as@andre-simon.de>
2023-08-11 16:17:04 +0200

    updated version and notes

--- AStyle/src/astyle_main.cpp
+++ AStyle/src/astyle_main.cpp
@@ -94,7 +94,7 @@ namespace astyle {
 	jmethodID g_mid;
 #endif
 
-const char* g_version = "3.4.2";
+const char* g_version = "3.4.3";
 
 //-----------------------------------------------------------------------------
 // ASStreamIterator class
