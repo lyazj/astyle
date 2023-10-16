e2870b5171becbd2cef0b6785595435f46f93b66
saalen <as@andre-simon.de>
2023-10-16 13:27:30 +0200

    https://sourceforge.net/p/astyle/bugs/557 fixed inserting space with combined --pad-oper and --align-pointer=type option

--- AStyle/src/ASFormatter.cpp
+++ AStyle/src/ASFormatter.cpp
@@ -1184,7 +1184,7 @@ std::string ASFormatter::nextLine()
 
 		// #126
 		if ( currentChar == '*' && shouldPadOperators &&
-			pointerAlignment == PTR_ALIGN_NONE &&  // SF 557
+			pointerAlignment != PTR_ALIGN_TYPE &&  // SF 557
 			( currentHeader == &AS_IF || currentHeader == &AS_WHILE || currentHeader == &AS_DO || currentHeader == &AS_FOR)
 			&& ( previousChar == ')' || std::isalpha(previousChar) )
 			&& !isOperatorPaddingDisabled() ) {
