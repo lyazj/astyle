lyazj/astyle

commit 5146121caab719a9d3884d46305582a0c429f324

https://sourceforge.net/p/astyle/bugs/575 fix VS19 compiler warnings

AStyle/src/astyle_main.cpp
@@ -2409,8 +2409,7 @@ void ASConsole::processOptions(const std::vector<std::string>& argvOptions)
 			assert(strcmp(buf, "\xEF\xBB\xBF") == 0);
 		}
 		options.importOptions(optionsIn, fileOptionsVector);
-		ok = options.parseOptions(fileOptionsVector,
-		                          std::string(_("Invalid default options:")));
+		ok = options.parseOptions(fileOptionsVector);
 	}
 	else if (optionFileRequired)
 		error(_("Cannot open default option file"), optionFileName.c_str());
@@ -2436,8 +2435,7 @@ void ASConsole::processOptions(const std::vector<std::string>& argvOptions)
 			assert(strcmp(buf, "\xEF\xBB\xBF") == 0);
 		}
 		options.importOptions(projectOptionsIn, projectOptionsVector);
-		ok = options.parseOptions(projectOptionsVector,
-		                          std::string(_("Invalid project options:")));
+		ok = options.parseOptions(projectOptionsVector);
 	}
 
 	if (!ok)
@@ -2448,8 +2446,7 @@ void ASConsole::processOptions(const std::vector<std::string>& argvOptions)
 	}
 
 	// parse the command line options vector for errors
-	ok = options.parseOptions(optionsVector,
-	                          std::string(_("Invalid command line options:")));
+	ok = options.parseOptions(optionsVector);
 	if (!ok)
 	{
 		(*errorStream) << options.getOptionErrors();
@@ -2947,7 +2944,7 @@ ASOptions::ASOptions(ASFormatter& formatterArg, ASConsole& consoleArg)
  *
  * @return        true if no errors, false if errors
  */
-bool ASOptions::parseOptions(std::vector<std::string>& optionsVector, const std::string& errorInfo)
+bool ASOptions::parseOptions(std::vector<std::string>& optionsVector)
 {
 	std::vector<std::string>::iterator option;
 	std::string arg;
@@ -2959,7 +2956,7 @@ bool ASOptions::parseOptions(std::vector<std::string>& optionsVector, const std:
 		arg = *option;
 
 		if (arg.compare(0, 2, "--") == 0)
-			parseOption(arg.substr(2), errorInfo);
+			parseOption(arg.substr(2));
 		else if (arg[0] == '-')
 		{
 			size_t i;
@@ -2971,19 +2968,19 @@ bool ASOptions::parseOptions(std::vector<std::string>& optionsVector, const std:
 				        && arg[i - 1] != 'x')
 				{
 					// parse the previous option in subArg
-					parseOption(subArg, errorInfo);
+					parseOption(subArg);
 					subArg = "";
 				}
 				// append the current option to subArg
 				subArg.append(1, arg[i]);
 			}
 			// parse the last option
-			parseOption(subArg, errorInfo);
+			parseOption(subArg);
 			subArg = "";
 		}
 		else
 		{
-			parseOption(arg, errorInfo);
+			parseOption(arg);
 			subArg = "";
 		}
 	}
@@ -2992,7 +2989,7 @@ bool ASOptions::parseOptions(std::vector<std::string>& optionsVector, const std:
 	return true;
 }
 
-void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo)
+void ASOptions::parseOption(const std::string& arg)
 {
 	NegationPaddingMode negationPaddingMode = NEGATION_PAD_NO_CHANGE;
 	IncludeDirectivePaddingMode includeDirectivePaddingMode = INCLUDE_PAD_NO_CHANGE;
@@ -3099,7 +3096,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (spaceNumParam.length() > 0)
 			spaceNum = atoi(spaceNumParam.c_str());
 		if (spaceNum < 2 || spaceNum > 20)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 		{
 			formatter.setTabIndentation(spaceNum, false);
@@ -3116,7 +3113,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (spaceNumParam.length() > 0)
 			spaceNum = atoi(spaceNumParam.c_str());
 		if (spaceNum < 2 || spaceNum > 20)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 		{
 			formatter.setTabIndentation(spaceNum, true);
@@ -3133,7 +3130,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (tabNumParam.length() > 0)
 			tabNum = atoi(tabNumParam.c_str());
 		if (tabNum < 2 || tabNum > 20)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 		{
 			formatter.setForceTabXIndentation(tabNum);
@@ -3150,7 +3147,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (spaceNumParam.length() > 0)
 			spaceNum = atoi(spaceNumParam.c_str());
 		if (spaceNum < 2 || spaceNum > 20)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 		{
 			formatter.setSpaceIndentation(spaceNum);
@@ -3167,9 +3164,9 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (contIndentParam.length() > 0)
 			contIndent = atoi(contIndentParam.c_str());
 		if (contIndent < 0)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else if (contIndent > 4)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 			formatter.setContinuationIndentation(contIndent);
 	}
@@ -3180,7 +3177,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (minIndentParam.length() > 0)
 			minIndent = atoi(minIndentParam.c_str());
 		if (minIndent >= MINCOND_END)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 			formatter.setMinConditionalIndentOption(minIndent);
 	}
@@ -3191,7 +3188,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (maxIndentParam.length() > 0)
 			maxIndent = atoi(maxIndentParam.c_str());
 		if (maxIndent < 40)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 			formatter.setMaxContinuationIndentLength(maxIndent);
 	}
@@ -3311,7 +3308,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (keepEmptyLinesParam.length() > 0)
 			keepEmptyLines = atoi(keepEmptyLinesParam.c_str());
 		if (keepEmptyLines < 1)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 			formatter.setSqueezeEmptyLinesNumber(keepEmptyLines);
 	}
@@ -3383,7 +3380,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (styleParam.length() > 0)
 			align = atoi(styleParam.c_str());
 		if (align < 1 || align > 3)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else if (align == 1)
 			formatter.setPointerAlignment(PTR_ALIGN_TYPE);
 		else if (align == 2)
@@ -3414,7 +3411,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (styleParam.length() > 0)
 			align = atoi(styleParam.c_str());
 		if (align < 0 || align > 3)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else if (align == 0)
 			formatter.setReferenceAlignment(REF_ALIGN_NONE);
 		else if (align == 1)
@@ -3431,9 +3428,9 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (maxLengthParam.length() > 0)
 			maxLength = atoi(maxLengthParam.c_str());
 		if (maxLength < 50)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else if (maxLength > 200)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 			formatter.setMaxCodeLength(maxLength);
 	}
@@ -3444,7 +3441,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		if (maxLengthParam.length() > 0)
 			maxLength = atoi(maxLengthParam.c_str());
 		if (maxLength > 200)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else
 			formatter.setMaxCodeLength(maxLength);
 	}
@@ -3493,9 +3490,9 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 		formatter.setAttachReturnTypeDecl(true);
 	}
 	// To avoid compiler limit of blocks nested too deep.
-	else if (!parseOptionContinued(arg, errorInfo))
+	else if (!parseOptionContinued(arg))
 	{
-		isOptionError(arg, errorInfo);
+		isOptionError(arg);
 	}
 
 	formatter.setNegationPaddingMode(negationPaddingMode);
@@ -3507,7 +3504,7 @@ void ASOptions::parseOption(const std::string& arg, const std::string& errorInfo
 // To avoid compiler limit of blocks nested too deep.
 // Return 'true' if the option was found and processed.
 // Return 'false' if the option was not found.
-bool ASOptions::parseOptionContinued(const std::string& arg, const std::string& errorInfo)
+bool ASOptions::parseOptionContinued(const std::string& arg)
 {
 	// Objective-C options
 	if (isOption(arg, "xQ", "pad-method-prefix"))
@@ -3637,7 +3634,7 @@ bool ASOptions::parseOptionContinued(const std::string& arg, const std::string& 
 		if (lineendParam.length() > 0)
 			lineendType = atoi(lineendParam.c_str());
 		if (lineendType < 1 || lineendType > 3)
-			isOptionError(arg, errorInfo);
+			isOptionError(arg);
 		else if (lineendType == 1)
 			formatter.setLineEndFormat(LINEEND_WINDOWS);
 		else if (lineendType == 2)
@@ -3752,10 +3749,10 @@ bool ASOptions::isOption(const std::string& arg, const char* op1, const char* op
 	return (isOption(arg, op1) || isOption(arg, op2));
 }
 
-void ASOptions::isOptionError(const std::string& arg, const std::string& errorInfo)
+void ASOptions::isOptionError(const std::string& arg)
 {
 	if (optionErrors.str().length() == 0)
-		optionErrors << errorInfo << std::endl;   // need main error message
+		optionErrors << "Invalid Artistic Style options:" << std::endl;   // need main error message
 	optionErrors << "\t" << arg << std::endl;
 }
 

AStyle/src/ASBeautifier.cpp
@@ -762,7 +762,7 @@ std::string ASBeautifier::beautify(const std::string& originalLine)
 	size_t iPrelim = headerStack->size();
 
 	// calculate preliminary indentation based on headerStack and data from past lines
-	computePreliminaryIndentation(line);
+	computePreliminaryIndentation();
 
 	// parse characters in the current line.
 	parseCurrentLine(line);
@@ -1458,7 +1458,7 @@ void ASBeautifier::registerContinuationIndentColon(std::string_view line, int i,
  */
 std::pair<int, int> ASBeautifier::computePreprocessorIndent()
 {
-	computePreliminaryIndentation("");
+	computePreliminaryIndentation();
 	std::pair<int, int> entry(indentCount, spaceIndentCount);
 	if (!headerStack->empty()
 	        && entry.first > 0
@@ -2102,7 +2102,7 @@ void ASBeautifier::processPreprocessor(std::string_view preproc, std::string_vie
 // Compute the preliminary indentation based on data in the headerStack
 // and data from previous lines.
 // Update the class variable indentCount.
-void ASBeautifier::computePreliminaryIndentation(std::string_view line)
+void ASBeautifier::computePreliminaryIndentation()
 {
 	indentCount = 0;
 	spaceIndentCount = 0;

AStyle/src/ASFormatter.cpp
@@ -1952,7 +1952,7 @@ std::string ASFormatter::nextLine()
 			}
 
 			if (shouldPadParensOutside || shouldPadParensInside || shouldUnPadParens || shouldPadFirstParen)
-				padParensOrBrackets('(', ')', shouldPadParensOutside, shouldPadParensInside, shouldUnPadParens, shouldPadFirstParen);
+				padParensOrBrackets('(', ')', shouldPadFirstParen);
 			else
 				appendCurrentChar();
 
@@ -1982,7 +1982,7 @@ std::string ASFormatter::nextLine()
 		bool isDoubleOpenBrackets = isGHCStyle() && currentChar=='[' && peekNextChar() == '[';
 		if ((currentChar == '[' || currentChar == ']' ) && (shouldPadBracketsOutside || shouldPadBracketsInside || shouldUnPadBrackets) && !isDoubleOpenBrackets)
 		{
-			padParensOrBrackets('[', ']', shouldPadBracketsOutside, shouldPadBracketsInside, shouldUnPadBrackets, false);
+			padParensOrBrackets('[', ']', false);
 			continue;
 		}
 
@@ -4799,7 +4799,7 @@ void ASFormatter::formatPointerOrReferenceCast()
  * the parens and necessary padding will be appended to formattedLine
  * the calling function should have a continue statement after calling this method
  */
-void ASFormatter::padParensOrBrackets(char openDelim, char closeDelim, bool shouldPadParensOutside, bool shouldPadParensInside, bool shouldUnPadParens, bool shouldPadFirstParen)
+void ASFormatter::padParensOrBrackets(char openDelim, char closeDelim, bool padFirstParen)
 {
 	assert(currentChar == openDelim || currentChar == closeDelim);
 
@@ -4884,7 +4884,7 @@ void ASFormatter::padParensOrBrackets(char openDelim, char closeDelim, bool shou
 
 		// pad open paren outside
 		char peekedCharOutside = peekNextChar();
-		if (shouldPadFirstParen && ( (previousChar != openDelim && peekedCharOutside != closeDelim)  || shouldPadEmptyParens ) )
+		if (padFirstParen && ( (previousChar != openDelim && peekedCharOutside != closeDelim)  || shouldPadEmptyParens ) )
 			appendSpacePad();
 		else if (shouldPadParensOutside)
 		{

AStyle/src/astyle.h
@@ -440,7 +440,7 @@ private:  // functions
 	void adjustObjCMethodDefinitionIndentation(std::string_view line_);
 	void adjustObjCMethodCallIndentation(std::string_view line_);
 	void adjustParsedLineIndentation(size_t iPrelim, bool isInExtraHeaderIndent);
-	void computePreliminaryIndentation(std::string_view line);
+	void computePreliminaryIndentation();
 	void parseCurrentLine(std::string_view line);
 	void popLastContinuationIndent();
 	void processPreprocessor(std::string_view preproc, std::string_view line);
@@ -872,7 +872,7 @@ private:  // functions
 	void padObjCParamType();
 	void padObjCReturnType();
 	void padOperators(const std::string* newOperator);
-	void padParensOrBrackets(char openDelim, char closeDelim, bool shouldPadParensOutside, bool shouldPadParensInside, bool shouldUnPadParens, bool shouldPadFirstParen);
+	void padParensOrBrackets(char openDelim, char closeDelim, bool padFirstParen);
 	void processPreprocessor();
 	void resetEndOfStatement();
 	void setAttachClosingBraceMode(bool state);

AStyle/src/astyle_main.h
@@ -179,7 +179,7 @@ public:
 #endif
 	std::string getOptionErrors() const;
 	void importOptions(std::stringstream& in, std::vector<std::string>& optionsVector);
-	bool parseOptions(std::vector<std::string>& optionsVector, const std::string& errorInfo);
+	bool parseOptions(std::vector<std::string>& optionsVector);
 
 private:
 	// variables
@@ -194,11 +194,11 @@ private:
 	std::string getParam(const std::string& arg, const char* op1, const char* op2);
 	bool isOption(const std::string& arg, const char* op);
 	bool isOption(const std::string& arg, const char* op1, const char* op2);
-	void isOptionError(const std::string& arg, const std::string& errorInfo);
+	void isOptionError(const std::string& arg);
 	bool isParamOption(const std::string& arg, const char* option);
 	bool isParamOption(const std::string& arg, const char* option1, const char* option2);
-	void parseOption(const std::string& arg, const std::string& errorInfo);
-	bool parseOptionContinued(const std::string& arg, const std::string& errorInfo);
+	void parseOption(const std::string& arg);
+	bool parseOptionContinued(const std::string& arg);
 };
 
 #ifndef	ASTYLE_LIB

commit 2c9258da224ba9986aa5bf2eaa80266cad219b87

fix(astyle_main.cpp): fix parseOption() usage against a recent change
This method used to take two parameters, now it only takes one.

Fixes: 5146121caab719a9d3884d46305582a0c429f324

AStyle/src/astyle_main.cpp
@@ -4213,7 +4213,7 @@ extern "C" EXPORT char* STDCALL AStyleMain(const char* pSourceIn,		// the source
 
 	options.importOptions(opt, optionsVector);
 
-	bool ok = options.parseOptions(optionsVector, "Invalid Artistic Style options:");
+	bool ok = options.parseOptions(optionsVector);
 	if (!ok)
 		fpErrorHandler(130, options.getOptionErrors().c_str());
 