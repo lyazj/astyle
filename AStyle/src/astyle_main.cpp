lyazj/astyle

commit 8aeea822b9d34b3f83f983d395fb9797df9c98ab

https://gitlab.com/saalen/astyle/-/issues/43 keep initial EOL for file name input

AStyle/src/astyle_main.cpp
@@ -472,6 +472,10 @@ void ASConsole::formatFile(const std::string& fileName_)
 		out << nextLine;
 		linesOut++;
 
+		if (LINEEND_DEFAULT == formatter.getLineEndFormat()){
+			outputEOL = streamIterator.getLastOutputEOL();
+		}
+
 		if (formatter.hasMoreLines())
 		{
 			out << outputEOL;

commit 7e0e06dca5dd012499812f581d7582e30bb44767

fix(astyle_main.cpp): fix build against a recent change
Fixes: 8aeea822b9d34b3f83f983d395fb9797df9c98ab

AStyle/src/astyle_main.cpp
@@ -4226,13 +4226,13 @@ extern "C" EXPORT char* STDCALL AStyleMain(const char* pSourceIn,		// the source
 	{
 		out << formatter.nextLine();
 		if (formatter.hasMoreLines())
-			out << streamIterator.getOutputEOL();
+			out << streamIterator.getLastOutputEOL();
 		else
 		{
 			// this can happen if the file if missing a closing brace and break-blocks is requested
 			if (formatter.getIsLineReady())
 			{
-				out << streamIterator.getOutputEOL();
+				out << streamIterator.getLastOutputEOL();
 				out << formatter.nextLine();
 			}
 		}